a1c969d492d0088d2ad22483b06b09bd3b8269ef
umersajjad32 <umersajjad3232@gmail.com>
2022-12-15 15:32:14 +0500

    Changes

--- app.py
+++ app.py
@@ -26,21 +26,23 @@ def run():
     st.sidebar.info('This app is created to predict happiness of countries')
     st.sidebar.success('Created by: Muhammad Umer Sajjad')
 
-    st.sidebar.image(image_hospital)
+    st.sidebar.image(image_profile)
 
     st.title("Countries Happiness Prediction App")
 
     if add_selectbox == 'Online':
 
-        GDP_per_Capita = st.number_input('Log of GDP_per_Capita', min_value=0.00, max_value=2.30, value=0.97)
-        Social_Support = st.number_input('Annual Avg of Social_Support', min_value=0.00, max_value=1.74, value=1.03)
-        Healthy_Life_Expectancy = st.number_input('Healthy Life Expectancy', min_value=0.00, max_value=1.24,
+        # GDP_per_Capita = st.slider('How old are you?', 0, 130, 25)
+        GDP_per_Capita = st.slider('Log of GDP_per_Capita', min_value=0.00, max_value=2.30, value=0.97)
+        # GDP_per_Capita = st.number_input('Log of GDP_per_Capita', min_value=0.00, max_value=2.30, value=0.97)
+        Social_Support = st.slider('Annual Avg of Social_Support', min_value=0.00, max_value=1.74, value=1.03)
+        Healthy_Life_Expectancy = st.slider('Healthy Life Expectancy', min_value=0.00, max_value=1.24,
                                                   value=0.60)
-        Freedom = st.number_input('Annual Avg of Freedom', min_value=0.00, max_value=0.84, value=0.44)
-        Government_Corruption_Perception = st.number_input('Annual Avg of Government Corruption Perception', min_value=0.00,
+        Freedom = st.slider('Annual Avg of Freedom', min_value=0.00, max_value=0.84, value=0.44)
+        Government_Corruption_Perception = st.slider('Annual Avg of Government Corruption Perception', min_value=0.00,
                                                            max_value=0.68, value=0.13)
-        Generosity = st.number_input('Annual Avg of Generosity', min_value=0.00, max_value=0.93, value=0.20)
-        Regional_Indicator = st.selectbox('Region of Your Country', [west_europe,north_america,middle_east,latin_america,central_europe,east_asia,south_east_asia,commonwealth_independent,sub_saharan_africa,south_asia])
+        Generosity = st.slider('Annual Avg of Generosity', min_value=0.00, max_value=0.93, value=0.20)
+        Regional_Indicator = st.selectbox('Region of Your Country', ['west_europe','north_america','middle_east','latin_america','central_europe','east_asia','south_east_asia','commonwealth_independent','sub_saharan_africa','south_asia'])
 
         #         if st.checkbox('Smoker'):
         #             smoker = 'yes'
